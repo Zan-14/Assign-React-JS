38ae01d5603e3500bda353df7fdaf16a0d483c43
Zan-14 <fauzanjumdinharm@gmail.com>
2023-09-24 08:05:19 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+const App = () => {
+  return (
+    <div className="font-bold text-2xl underline text-center m-10">
+      App test tailwind
+    </div>
+  );
+};
+
+export default App;
